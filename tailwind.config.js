a42a60a766a9019ee545425b92c88a9e9d5c4981
khalid@skywalker <idk645912@gmail.com>
2025-07-31 06:32:11 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        starYellow: "#FFD500",
+        starBlack: "#000000",
+      },
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
