ef2f319635a256f574db5f3c74c065081b34db0c
khalid@skywalker <idk645912@gmail.com>
2025-07-31 06:33:25 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function App() {
+  return (
+    <div style={{textAlign:'center',padding:'50px'}}>
+      <h1 style={{color:'#FFD500'}}>🚀 Khalid@Skywalker Portfolio</h1>
+      <p>This is your Star Wars-themed React portfolio placeholder.</p>
+    </div>
+  );
+}
